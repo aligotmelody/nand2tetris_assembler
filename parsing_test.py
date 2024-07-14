6e260d2d47bb362f77856a0377c84decd4db77c5
Ali Eltaib <alieltaib71@gmail.com>
2024-07-14 05:55:28 -0400

    Cinstruct are breaking

--- parsing_test.py
+++ parsing_test.py
@@ -1,46 +1,51 @@
 from ply import lex
 import termcolor
+import re
 
-
+Ainst_address = re.compile(r'[0-9]+')
+def if_address(Ainstruct):
+      return bool(Ainst_address.match(Ainstruct))
 
 tokens = (
   "LABEL",
-  "VARIABLES",
   "ADDRESSES",
   "CINSTRUCTION",
   "AINSTRUCTION",
   "COMMENTS"
 )
 
-t_LABEL = r'\([A-Za-z0-9]+\)'
-t_VARIABLES = r'\@[A-Za-z]+(?:[A-Za-z0-9]+)?'
+t_LABEL = r'\((?:[A-Za-z0-9]+\s*(?:[A-Za-z0-9]+)?)*\)'
 t_ADDRESSES = r'\@[0-9]+'
-t_AINSTRUCTION = r'\@[A-Za-z0-9]+'
-t_CINSTRUCTION = r'[A-Za-z0-9]{1,4}\s*(?:[=|;])\s*(?:(?:!|-)?(?:[A-Za-z0-9]+))?\s*(?:(?:\||\+|-)?(?:[A-Za-z0-9])?)|;(?:[A-Za-z0-9]{1,4})? '
-#                  [A-Za-z]{1,4}\s*=\s*(?:(?:!|-)?([A-Za-z0-9])?)?
-#                  [A-Za-z]{1,4}\s*=\s*(?:(?:!|-)?(?:[A-Za-z0-9]|\(.*?\))?)*
-#|;(?:[A-Za-z0-9]{1,4})?|[A-Za-z]\s*(?:[&\|\+\-])\s*[A-Za-z0-9]'
-t_ignore = '\t\r\n\f\v '  # Ignore whitespace
-
+t_AINSTRUCTION = r'\@(?:[A-Za-z0-9]+)'
+t_CINSTRUCTION = r'[A-Za-z0]{1,3}\s{0,2}(?:[=|;])\s{0,2}(?:(?:!|-)?)\s{0,2}(?:[A-Za-z0-9]{1,4})\s{0,2}(?:(?:\||\+|\-|&)?(?:[A-Za-z0-9])?)|;(?:[A-Za-z0-9]{1,3})? '
+t_ignore = '\t\r\f\v '  # Ignore whitespace
 t_COMMENTS = r'//.*'
+
+lex.lex(debug=0, optimize=False, reflags=re.DOTALL)
+
+def t_NEWLINE(t):
+    r'\n+'
+    t.lexer.lineno += len(t.value)
+    pass
+    
+
 def t_error(t):
   print(f"Illegal character '{t.value[0]}' at line {t.lexer.lineno}")
   t.lexer.skip(1)
+  
+
+
 
 
 
 def parse_source_code(source_code):
+  #lex.lex(debug=0, optimize=False, reflags=re.DOTALL)
   lexer = lex.lex()
   lexer.input(source_code)
   labels = []
-  Vars = []
-  Addresses = []
-  Ainstruction =[]
-  Cinstruction ={
-    "dest": "" ,
-    "cmp" : "" ,
-    "jmp" : "" ,
-  }
+  Ainstr_Address = []
+  Ainst_var =[]
+  Cinstruction = []
   N_Cinstruction = {
         "dest": "dest" ,
         "cmp" : "cmp" ,
@@ -54,29 +59,34 @@ def parse_source_code(source_code):
       labels.append(tok.value[1:-1])  # Remove Parenthesis from label
       print(tok.value)
     elif tok.type == "AINSTRUCTION":
+        Ainstruct = (tok.value.strip("@"))
         print(termcolor.colored("Ains", "green"))
         print(tok.value)
-        #if  tok.type == "VARIABLES":
-         # print(termcolor.colored("var", "blue"))
-          #Vars.append(tok.value.strip("@"))
-        #elif tok.type == "ADDRESSES":
-         # print(termcolor.colored("ADDRESSES", "red"))
-          #Addresses.append(tok.value.strip("@"))
-        #else:
-         # print(f"there's something wrong with this character {tok.value}")
-
+        if  if_address(Ainstruct):
+           print(termcolor.colored("Address", "blue"))
+           Ainstr_Address.append(tok.value.strip("@"))
+        else:
+           print(f"the command is {tok.value}")
+           Ainst_var.append(tok.value.strip("@"))
     elif tok.type == "CINSTRUCTION":
       print(termcolor.colored("CINSTRUCTION", "magenta"))
+      Cinstruction.append(tok.value.strip("\n"))
       print(tok.value)
     elif tok.type == "COMMENTS":
       print(tok.value)
     else:
       continue
+
+  return labels, Ainst_var, Ainstr_Address, Cinstruction
    
 
 
+with open("ass_test.txt", "r") as F:
+  source = F.read()
+  
 
-source = """
+
+"""
 //n=2
 @2
 D=M
@@ -97,7 +107,14 @@ MD=-D
 D;JGT
 @R1     //using a label
 d=m
+(hello world)
+(my name is ali)
 
 """
-
-print(parse_source_code(source))
+#total labels 3, total vars 3, total Ainst_addre 2, Cinstr 13
+LAB, VARS, ADDRESSES, CINSTR= parse_source_code(source)
+print(LAB)
+print(VARS)
+print(ADDRESSES)
+print(CINSTR)
+print(len(CINSTR))
--- source_parser.py
+++ source_parser.py
@@ -1,61 +1,77 @@
 from ply import lex
+import termcolor
+
+
 
 tokens = (
   "LABEL",
-  "Variabels",
-  "Comments",
-  "Ainstruction",
-  "Cinstruction",
+  "VARIABLES",
+  "ADDRESSES",
+  "CINSTRUCTION",
+  "AINSTRUCTION",
+  "COMMENTS"
 )
 
-t_LABEL = r"\([A-Za-z0-9]+\):"
-t_Ainstruction = r"\@[A-Za-z0-9]+"
-t_Cinstruction = r"\@([A-Za-z0-9]+|\||&|\+|;|=[A-Za-z0-9])"
-t_Variabels = r"[A-Za-z]+"
-t_Comments = r"//.*"
-t_ignore = " \t\n"  # Ignore whitespace
+t_LABEL = r'\([A-Za-z0-9]+\)'
+t_VARIABLES = r'\@[A-Za-z]+(?:[A-Za-z0-9]+)?'
+t_ADDRESSES = r'\@[0-9]+'
+t_AINSTRUCTION = r'\@[A-Za-z0-9]+'
+t_CINSTRUCTION = r'[A-Za-z0-9]{1,4}\s*(?:[=|;])\s*(?:(?:!|-)?(?:[A-Za-z0-9]+))?\s*(?:(?:\||\+|-)?(?:[A-Za-z0-9])?)|;(?:[A-Za-z0-9]{1,4})? '
+#                  [A-Za-z]{1,4}\s*=\s*(?:(?:!|-)?([A-Za-z0-9])?)?
+#                  [A-Za-z]{1,4}\s*=\s*(?:(?:!|-)?(?:[A-Za-z0-9]|\(.*?\))?)*
+#|;(?:[A-Za-z0-9]{1,4})?|[A-Za-z]\s*(?:[&\|\+\-])\s*[A-Za-z0-9]'
+t_ignore = '\t\r\n\f\v '  # Ignore whitespace
 
+t_COMMENTS = r'//.*'
 def t_error(t):
   print(f"Illegal character '{t.value[0]}' at line {t.lexer.lineno}")
   t.lexer.skip(1)
+
+
 
 def parse_source_code(source_code):
   lexer = lex.lex()
   lexer.input(source_code)
   labels = []
+  Vars = []
+  Addresses = []
   Ainstruction =[]
   Cinstruction ={
     "dest": "" ,
     "cmp" : "" ,
     "jmp" : "" ,
   }
+  N_Cinstruction = {
+        "dest": "dest" ,
+        "cmp" : "cmp" ,
+        "jmp" : "jmp" ,
+      }
   while True:
     tok = lexer.token()
     if not tok:
-      
       break
     if tok.type == "LABEL":
       labels.append(tok.value[1:-1])  # Remove Parenthesis from label
-      print(tok)
-    elif tok.type == "Ainstruction":
-      Ainstruction.append(tok.value.strip("@"))
-    elif tok.type == "Cinstruction":
-      Cinst_dest_rest = (tok.value.split("="))
-      print(Cinst_dest_rest)
-      Cinst_cm_jm = Cinst_dest_rest.split(";")
-      dest = Cinst_dest_rest[0]
-      cmp  = Cinst_cm_jm[0]
-      jmp  = Cinst_cm_jm[1]
-      N_Cinstruction = {
-        "dest": dest ,
-        "cmp" : cmp ,
-        "jmp" : jmp ,
-      }
-      Cinstruction.update(N_Cinstruction)
+      print(tok.value)
+    elif tok.type == "AINSTRUCTION":
+       # print(termcolor.colored("Ains", "green"))
+        print(tok.value)
+        #if  tok.type == "VARIABLES":
+         # print(termcolor.colored("var", "blue"))
+          #Vars.append(tok.value.strip("@"))
+        #elif tok.type == "ADDRESSES":
+         # print(termcolor.colored("ADDRESSES", "red"))
+          #Addresses.append(tok.value.strip("@"))
+        #else:
+         # print(f"there's something wrong with this character {tok.value}")
 
-
-
-  return labels, Ainstruction, Cinstruction
+    elif tok.type == "CINSTRUCTION":
+     # print(termcolor.colored("CINSTRUCTION", "magenta"))
+      print(tok.value)
+    elif tok.type == "COMMENTS":
+      print(tok.value)
+    else:
+      continue
 
 source = """
 //n=2
